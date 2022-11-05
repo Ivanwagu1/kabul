fd8eef487cc566c6a3188e04d3ba6161a67ef30c
Ivanwagu1 <115166614+Ivanwagu1@users.noreply.github.com>
2022-11-05 11:07:02 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -7,7 +7,7 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup || db.data.settings[th
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
     let wan = 'WannBotz-MD'
-    let caption = `Halo ${name}, Saya Adalah ${namebot}, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${namebot}, ʙᴀɴᴛᴜ?\nᴋᴇᴛɪᴋ *.ᴍᴇɴᴜ* ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ sᴇᴍᴜᴀ ғɪᴛᴜʀ ʙᴏᴛ.\n\nᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ᴊᴏɪɴ ᴍᴀsᴜᴋ ɢʀᴏᴜᴘ\n\nɢʀᴏᴜᴘ ᴢʏᴋᴏʙᴏᴛ-ᴍᴅ ᴏғғɪᴄɪᴀʟ :\nhttps://chat.whatsapp.com/B9PTc9PZbBH0INkdEN07ws`}
+    let caption = `Halo ${name}, Saya Adalah ${namebot}, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${namebot}, ʙᴀɴᴛᴜ?\nᴋᴇᴛɪᴋ *.ᴍᴇɴᴜ* ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ sᴇᴍᴜᴀ ғɪᴛᴜʀ ʙᴏᴛ.\n\nᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ᴊᴏɪɴ ᴍᴀsᴜᴋ ɢʀᴏᴜᴘ\n\nɢʀᴏᴜᴘ 𝓢𝓐𝓝𝓢-ᴍᴅ ᴏғғɪᴄɪᴀʟ :\nhttps://chat.whatsapp.com/EPmjuw9X1wZDGX1p4NCV1e`}
 `.trim()
     this.sendButton(m.chat, caption, wm, null, [[user.banned ? 'Owner' : 'Menu', user.banned ? '/owner' : '/menu']], m, { mentions: this.parseMention(caption) })
     user.pc = new Date * 1
